eb48e8aef8a9cd5ff245ab23502463e00f7e0193
dziljalal1 <dziljalal1@gmail.com>
2021-06-19 05:04:43 +0700

    Sebelum coba purge css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-module.exports = {
-  purge: [],
-  // purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+module.exports = {  
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: "media", // or 'media' or 'class'
   theme: {
     extend: {},
